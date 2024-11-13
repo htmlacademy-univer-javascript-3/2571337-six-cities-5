878ea8802dee74c66857708d7fd7f3553c7febbb
Olivka07 <uchenik.ikt@yandex.ru>
2024-11-13 21:36:42 +0300

    fix conflicts

--- src/utils/toStringOrNumber.ts
+++
@@ -1,3 +0,0 @@
-export function toStringOrNumber<T extends object> (obj: T, key: string, value: string | number) {
-  return typeof obj[key as keyof T] === 'number' ? Number(value) : String(value);
-}
